ee6ad10b6e06bfa20c16be0429dcd2cb81fab379
Матвей Липилин <matvey.lipilin1@gmail.com>
2020-04-28 20:18:54 +0500

    added run params and fixed screen drawing

--- application.py
+++ application.py
@@ -7,55 +7,35 @@ BACKGROUND_COLOR = (0, 0, 0)
 
 
 class Application:
+    from ecs.world import World
+    from main_timer import Time
     __slots__ = ("__screen", "__is_running", "__clock")
-    __instance: "Application" = None
 
     def __init__(self):
-
-        from ecs.world import World
-
-        if Application.__instance is None:
+        if not pygame.get_init():
             pygame.init()
-            Application.__instance = self
-        else:
-            raise Exception()
 
         self.__is_running = False
         self.__screen: pygame.Surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF)
         self.__clock = pygame.time.Clock()
 
-        World.default_world = World()
+        Application.World.default_world = Application.World()
 
-    def run(self, profile: bool = False, clear_log: bool = False) -> None:
-
-        from ecs.world import World
-        from main_timer import Time
-
-        if profile:
-            Profiler.begin_profile_session()
-
-        World.default_world.create_all_systems()
+    def run(self) -> None:
+        Profiler.begin_profile_session()
 
         self.__is_running = True
         while self.__is_running:
             self.__screen.fill(BACKGROUND_COLOR)
 
-            World.update_current()
+            Application.World.update_current()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.__is_running = False
                     break
-            pygame.display.flip()
-            Time.tick()
-            pygame.display.set_caption(str(int(Time.get_fps())))
 
-        if clear_log:
-            Profiler.clear_log()
-        if profile:
-            Profiler.end_profile_session()
+            Application.Time.tick()
 
+        Profiler.end_profile_session()
         pygame.quit()
 
-    @staticmethod
-    def get_render_surface() -> pygame.Surface:
-        return Application.__instance.__screen
