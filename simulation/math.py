50f8aad862d763c075ca10d525f56c65dfe4348b
Матвей Липилин <matvey.lipilin1@gmail.com>
2020-05-03 18:43:44 +0500

    fixed operators added str and repr added division

--- simulation/math.py
+++ simulation/math.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 class Vector:
     __slots__ = ("x", "y")
     x: float
@@ -7,8 +10,6 @@ class Vector:
         self.x = x
         self.y = y
 
-    from typing import Tuple
-
     def to_tuple(self) -> Tuple[float, float]:
         return self.x, self.y
 
@@ -16,9 +17,16 @@ class Vector:
     def clone(vector: "Vector") -> "Vector":
         return Vector(vector.x, vector.y)
 
+    def __repr__(self):
+        return "Vector" + self.__str__()
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
     def __iadd__(self, other: "Vector"):
         self.x += other.x
         self.y += other.y
+        return self
 
     def __add__(self, other: "Vector"):
         vec = Vector.clone(self)
@@ -28,6 +36,7 @@ class Vector:
     def __isub__(self, other: "Vector"):
         self.x -= other.x
         self.y -= other.y
+        return self
 
     def __sub__(self, other: "Vector"):
         vec = Vector.clone(self)
@@ -37,8 +46,22 @@ class Vector:
     def __imul__(self, other: float):
         self.x *= other
         self.y *= other
+        return self
 
     def __mul__(self, other: float):
         vec = Vector.clone(self)
         vec *= other
         return vec
+
+    def __itruediv__(self, other: float):
+        self.x /= other
+        self.y /= other
+        return self
+
+    def __truediv__(self, other: float):
+        vec = Vector.clone(self)
+        vec /= other
+        return vec
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
