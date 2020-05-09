17dbc056a66d62c4479d8da5df5e5676037dff8e
Матвей Липилин <matvey.lipilin1@gmail.com>
2020-05-09 13:02:01 +0500

    improved components removing and entities deletion systems now have update order

--- ecs/systems.py
+++ ecs/systems.py
@@ -6,10 +6,10 @@ from typing import Set
 
 
 class BaseSystem(ABC):
-    __slots__ = ("is_enabled", "entity_manager", "filter")
+    __slots__ = ("is_enabled", "entity_manager")
     is_enabled: bool
-    filter: ComponentDataFilter
     entity_manager: EntityManager
+    __update_order__: int = 0
 
     def on_create(self) -> None:
         pass
@@ -18,5 +18,8 @@ class BaseSystem(ABC):
     def on_update(self, delta_time: float) -> None:
         raise NotImplementedError()
 
-    def query(self) -> Set[ComponentDataArray]:
-        return self.entity_manager.get_entities().filter(self.filter)
+    def query(self, data_filter: ComponentDataFilter) -> Set[ComponentDataArray]:
+        return self.entity_manager.get_entities().filter(data_filter)
+
+    def __lt__(self, other: "BaseSystem"):
+        return self.__update_order__ < other.__update_order__
