db83af1644d57756c3e5587f321abcf5f9140b15
Матвей Липилин <matvey.lipilin1@gmail.com>
2020-05-10 22:02:49 +0500

    added new commands

--- vk_bot/commands.py
+++ vk_bot/commands.py
@@ -1,24 +1,51 @@
 from abc import ABC, abstractmethod
 from random import randint
-from ecs.world import World
 from typing import Tuple
+import requests
+import json
+
+from ecs.world import World
 
 
 class BotMethods:
     from vk_api import VkApi
-    __slots__ = ("__api", "__uploads")
+    __slots__ = ("__api",)
+    __instance: "BotMethods" = None
 
     def __init__(self, session: VkApi):
-        from vk_api.upload import VkUpload
-        self.__api = session.get_api()
-        self.__uploads = VkUpload(session)
+        if BotMethods.__instance is None:
+            BotMethods.__instance = self
+            self.__api = session.get_api()
 
-    def send_message(self, peer_id: str, message: str) -> None:
-        self.__api.messages.send(peer_id=peer_id, message=message, random_id=self.random_id())
+    @staticmethod
+    def send_message(peer_id: str, message: str) -> None:
+        BotMethods.__instance.__api.messages.send(peer_id=peer_id, message=message, random_id=BotMethods.random_id())
 
     @staticmethod
     def random_id() -> str:
         return str(randint(-10000000, 10000000))
+
+    @staticmethod
+    def send_photo(peer_id: str, file_path: str) -> None:
+        try:
+            api = BotMethods.__instance.__api
+            data = api.photos.getMessagesUploadServer(user_id=peer_id)
+
+            upload_url = data["upload_url"]
+            files = {'photo': open(file_path, 'rb')}
+
+            response = requests.post(upload_url, files=files)
+            result = json.loads(response.text)
+
+            upload_result = api.photos.saveMessagesPhoto(server=result["server"],
+                                                         photo=result["photo"],
+                                                         hash=result["hash"])
+
+            api.messages.send(user_id=peer_id,
+                              attachment=f'photo{upload_result[0]["owner_id"]}_{upload_result[0]["id"]}',
+                              random_id=BotMethods.random_id())
+        except BaseException as e:
+            print(e)
 
 
 class BaseCommand(ABC):
@@ -71,7 +98,7 @@ class BaseCommand(ABC):
     @classmethod
     def help_data(cls):
         args = " ".join("{" + i[0] + "}" for i in cls._args) + " " if cls._args is not None else ""
-        return f"{cls._name} {args}— {cls._description}."
+        return f"{cls._name} {args} — {cls._description}."
 
 
 class HelloCommand(BaseCommand):
@@ -97,7 +124,7 @@ class CreateEntitiesCommand(BaseCommand):
         count = args["число"]
         entity_manager = World.current_world.get_manager()
         data_filter = entity_manager.create_filter(required=(Rigidbody,))
-        
+
         def buffered_command():
             c = max(0, min(count, MAX_CREATURES - len(entity_manager.get_entities().filter(data_filter))))
             for i in range(c):
@@ -128,42 +155,119 @@ class CreateUserEntityCommand(BaseCommand):
             entity = entity_manager.create_entity()
             create_named_creature(entity_manager, entity, name, randint(0, len(TEAM_COLORS)))
             id_comp = UserId()
-            id_comp.value = data["peer_id"]
+            id_comp.value = str(data["peer_id"])
             entity_manager.add_component(entity, id_comp)
 
-            methods.send_message(data["peer_id"], "Существо создано.")
+            methods.send_message(data["peer_id"], f"Существо создано. Его номер — {entity.get_id()}.")
 
         entity_manager.add_command(buffered_command)
+
+
+class ListCommand(BaseCommand):
+    _name = "list"
+    _description = "выводит список всех ваших существ"
+    _event_data = ("peer_id",)
+
+    def on_call(self, data: dict, args: dict, methods: BotMethods) -> None:
+        user_id = data["peer_id"]
+        entity_manager = World.current_world.get_manager()
+        message = []
+        try:
+            from simulation.components import UserId, EntityName
+
+            data_filter = entity_manager.create_filter(required=(EntityName, UserId))
+            for i in entity_manager.get_entities().filter(data_filter):
+                try:
+                    if i.get_component(UserId).value == str(user_id):
+                        message.append(f"{i.get_component(EntityName).value}, номер — {i.entity.get_id()}.")
+                except:
+                    continue
+            if not message:
+                message.append("У вас нет существ, создайте одно с помощью команды creature.")
+            methods.send_message(user_id, "\n".join(message))
+        except:
+            methods.send_message(user_id, "Что-то пошло не так.")
 
 
 class StatsCommand(BaseCommand):
     _name = "stats"
     _description = "показывает характеристики вашего существа"
+    _args = (("номер существа", int),)
     _event_data = ("peer_id",)
 
     def on_call(self, data: dict, args: dict, methods: BotMethods) -> None:
-        from simulation.components import UserId, MoveSpeed, Strength, Health, Hunger
         user_id = data["peer_id"]
         entity_manager = World.default_world.get_manager()
-        data_filter = entity_manager.create_filter(required=(UserId,),
-                                                   additional=(MoveSpeed, Strength, Health, Hunger))
         message = []
-        for i in entity_manager.get_entities().filter(data_filter):
-            if i.get_component(UserId).value != user_id:
-                continue
-            stat_comp = i.get_component(Health)
-            if stat_comp is not None:
-                message.append(f"Здоровье: {stat_comp.value}")
-            stat_comp = i.get_component(Strength)
-            if stat_comp is not None:
-                message.append(f"Сила: {stat_comp.value}")
-            stat_comp = i.get_component(MoveSpeed)
-            if stat_comp is not None:
-                message.append(f"Скорость: {stat_comp.value}")
-            stat_comp = i.get_component(Hunger)
-            if stat_comp is not None:
-                message.append(f"Сытость: {stat_comp.value}")
-            break
-        if not message:
-            message.append("Вы не создали сущесвто.\nСоздайте его с пошощью команды creature {имя}.")
-        methods.send_message(user_id, "\n".join(message))
+
+        num = args["номер существа"]
+        try:
+            from simulation.components import UserId, MoveSpeed, Strength, Health, Hunger
+            from simulation.systems import RenderSystem
+            from simulation.math import Vector
+            from ecs.entities import EntityNotFoundError
+
+            try:
+                entity = entity_manager.get_entity(num)
+                id_comp = entity_manager.get_component(entity, UserId)
+
+                if id_comp.value != str(user_id):
+                    raise EntityNotFoundError()
+
+                stat_comp = entity_manager.get_component(entity, Health)
+
+                if stat_comp is not None:
+                    message.append(f"Здоровье: {stat_comp.value}")
+                stat_comp = entity_manager.get_component(entity, Strength)
+                if stat_comp is not None:
+                    message.append(f"Сила: {stat_comp.value}")
+                stat_comp = entity_manager.get_component(entity, MoveSpeed)
+                if stat_comp is not None:
+                    message.append(f"Скорость: {stat_comp.value}")
+                stat_comp = entity_manager.get_component(entity, Hunger)
+                if stat_comp is not None:
+                    message.append(f"Сытость: {stat_comp.value}")
+
+                methods.send_message(user_id, "\n".join(message))
+            except:
+                methods.send_message(user_id, "Существо не найдено.")
+        except:
+            methods.send_message(user_id, "Что-то пошло не так.")
+
+
+class PhotoCommand(BaseCommand):
+    _name = "photo"
+    _description = "отправляет фото вашего существа"
+    _args = (("номер существа", int),)
+    _event_data = ("peer_id",)
+
+    def on_call(self, data: dict, args: dict, methods: BotMethods) -> None:
+        num = args["номер существа"]
+        user_id = data["peer_id"]
+        entity_manager = World.current_world.get_manager()
+        try:
+            from simulation.components import UserId, Position
+            from simulation.systems import RenderSystem
+            from simulation.math import Vector
+            from ecs.entities import EntityNotFoundError
+            from core.application import Application, WIDTH, HEIGHT
+            from pygame.image import save
+
+            try:
+                entity = entity_manager.get_entity(num)
+                pos = entity_manager.get_component(entity, Position).value
+                if entity_manager.get_component(entity, UserId).value != str(user_id):
+                    raise EntityNotFoundError()
+                render_system = World.current_world.get_system(RenderSystem)
+                render_system.camera_position = pos - Vector(WIDTH / 2, HEIGHT / 2)
+                render_surface = Application.get_render_surface()
+
+                def command():
+                    save(render_surface, "temp.jpg")
+                    methods.send_photo(user_id, "temp.jpg")
+
+                Application.add_post_render_command(lambda: entity_manager.add_command(command))
+            except:
+                methods.send_message(user_id, "Существо не найдено.")
+        except:
+            methods.send_message(user_id, "Что-то пошло не так.")
