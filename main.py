5ed687fa3db40624eb743227441645ddf2072fb8
Матвей Липилин <matvey.lipilin1@gmail.com>
2020-05-11 20:08:09 +0500

    added comments

--- main.py
+++ main.py
@@ -3,20 +3,32 @@ import src.sql.data as db_session
 import src.vk_bot.client
 import src.sql.core
 
-# Set to False if you don't want vk bot to broadcast messages
-BROADCAST_MESSAGES = False
+# Если 1, то бот будет всем участникам сообщества писать при включении и отключении.
+BROADCAST_MESSAGES = 0
 
 if __name__ == "__main__":
+
+    # Инициализация приложения.
     app = Application()
 
+    # Инициализация vk бота. С этого момента он отвечает на команды.
     src.vk_bot.client.init()
 
+    # Файл базы данных. Можно указать другое имя файла для другой симуляции.
     src.sql.core.init("entities.sqlite")
+
+    # Загрузка из базы данных.
     db_session.create_entities_from_database()
 
-    app.run(True, True)
+    # Главный цикл.
+    # Должен запускаться после всех инициализаций.
+    # profile=True - будет подсчитано время работы некоторых функций в папку profiling.
+    # clear_log=False - при True все предыдушие файлы в папке profiling будут удалены.
+    app.run(profile=True, clear_log=False)
 
+    # Сохранение всех существ в базу данных.
     db_session.save_to_database()
     db_session.close()
 
+    # Остановка всех потоков.
     Application.terminate()
